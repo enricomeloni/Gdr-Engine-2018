33a5637666ac8016f65f281fde0c8ad69a1ec78d
enricomeloni <enrico-meloni@hotmail.it>
2018-11-03 12:46:30 +0100

    Ad availability to RaceFactory

--- app/database/factories/RaceFactory.php
+++ app/database/factories/RaceFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Race::class, function (Faker $faker) {
     return [
-        'name' => $faker->colorName
+        'name' => $faker->colorName,
+        'availability' => $faker->numberBetween(0, 10)
     ];
 });
