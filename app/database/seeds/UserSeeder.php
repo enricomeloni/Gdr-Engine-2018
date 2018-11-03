15a7bd501c9402a04660c3e45b532141fd5b4d0e
enricomeloni <enrico-meloni@hotmail.it>
2018-11-03 11:24:07 +0100

    Create admin user at db creation

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -11,6 +11,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //factory(App\User::class, 1)->create();
+        $user = new App\User();
+        $user->name = "admin";
+        $user->password = bcrypt("alpine");
+        $user->email = "admin@admin.it";
+        $user->save();
+
+        $pc = factory(App\PlayingCharacter::class, 1)->create()->first();
+        $user->playingCharacters()->save($pc);
     }
 }
