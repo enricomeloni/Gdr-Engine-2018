967fb202b647599d4ea5f875a32166624910a7d3
enricomeloni <enrico-meloni@hotmail.it>
2019-02-23 00:01:23 +0100

    Add typescript support

---
+++ gdr-engine-net/gdr-engine-net/ClientApp/src/components/Test.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+
+interface Props {
+    test?: string
+}
+
+class Test extends React.Component<Props, {}> {
+    render() {
+        return <span> {this.props.test} </span>
+    }
+}
+
+export default Test;
--- gdr-engine-net/gdr-engine-net/ClientApp/src/index.tsx
+++ gdr-engine-net/gdr-engine-net/ClientApp/src/index.tsx
@@ -1,22 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory, BrowserHistoryBuildOptions } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
+import Test from './components/Test'
 import registerServiceWorker from './registerServiceWorker';
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const history = createBrowserHistory({ basename: baseUrl });
+const history = createBrowserHistory({ basename: baseUrl } as BrowserHistoryBuildOptions);
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
-const initialState = window.initialReduxState;
-const store = configureStore(history, initialState);
 
 const rootElement = document.getElementById('root');
+/*const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,5 +27,11 @@ ReactDOM.render(
     </ConnectedRouter>
   </Provider>,
   rootElement);
+  */
+
+ReactDOM.render(
+    <Test test="danielescemodimmerda" />,
+    rootElement
+)
 
 registerServiceWorker();
