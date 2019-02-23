7be2f31dc8959c9518b53ed7851289bb48845895
raffaele.zippo <raffaele.zippo@live.it>
2019-02-23 18:21:39 +0100

    Add GdrDbContext

---
+++ gdr-engine-net/GdrEngineNet.Database/GdrDbContext.cs
@@ -0,0 +1,28 @@
+﻿using gdr_engine_net.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GdrEngineNet.Database
+{
+    public class GdrDbContext : DbContext
+    {
+        public GdrDbContext(DbContextOptions<GdrDbContext> options) : base(options)
+        { }
+
+        DbSet<User> Users { get; set; }
+
+        DbSet<Room> Rooms { get; set; }
+
+        DbSet<Race> Races { get; set; }
+
+        DbSet<Character> Characters { get; set; }
+
+        DbSet<Class> Classes { get; set; }
+
+        DbSet<Guild> Guilds { get; set; }
+
+        DbSet<Item> Items { get; set; }
+    }
+}
