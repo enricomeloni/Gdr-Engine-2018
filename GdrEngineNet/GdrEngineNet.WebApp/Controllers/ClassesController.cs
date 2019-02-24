b1715cbb8690c01ae5118f9d8ca1958e15071dcf
raffaele.zippo <raffaele.zippo@live.it>
2019-02-24 19:02:41 +0100

    Add scaffolding controllers

---
+++ GdrEngineNet/GdrEngineNet.WebApp/Controllers/ClassesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GdrEngineNet.Database;
+using GdrEngineNet.Database.Models;
+
+namespace GdrEngineNet.Webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly GdrDbContext _context;
+
+        public ClassesController(GdrDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Classes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Class>>> GetClasses()
+        {
+            return await _context.Classes.ToListAsync();
+        }
+
+        // GET: api/Classes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Class>> GetClass(int id)
+        {
+            var @class = await _context.Classes.FindAsync(id);
+
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return @class;
+        }
+
+        // PUT: api/Classes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClass(int id, Class @class)
+        {
+            if (id != @class.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@class).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClassExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Classes
+        [HttpPost]
+        public async Task<ActionResult<Class>> PostClass(Class @class)
+        {
+            _context.Classes.Add(@class);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClass", new { id = @class.Id }, @class);
+        }
+
+        // DELETE: api/Classes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Class>> DeleteClass(int id)
+        {
+            var @class = await _context.Classes.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            _context.Classes.Remove(@class);
+            await _context.SaveChangesAsync();
+
+            return @class;
+        }
+
+        private bool ClassExists(int id)
+        {
+            return _context.Classes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GdrEngineNet/GdrEngineNet.WebApp/Controllers/RacesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GdrEngineNet.Database;
+using GdrEngineNet.Database.Models;
+
+namespace GdrEngineNet.Webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacesController : ControllerBase
+    {
+        private readonly GdrDbContext _context;
+
+        public RacesController(GdrDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Races
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Race>>> GetRaces()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        // GET: api/Races/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Race>> GetRace(int id)
+        {
+            var race = await _context.Races.FindAsync(id);
+
+            if (race == null)
+            {
+                return NotFound();
+            }
+
+            return race;
+        }
+
+        // PUT: api/Races/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRace(int id, Race race)
+        {
+            if (id != race.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(race).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RaceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Races
+        [HttpPost]
+        public async Task<ActionResult<Race>> PostRace(Race race)
+        {
+            _context.Races.Add(race);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRace", new { id = race.Id }, race);
+        }
+
+        // DELETE: api/Races/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Race>> DeleteRace(int id)
+        {
+            var race = await _context.Races.FindAsync(id);
+            if (race == null)
+            {
+                return NotFound();
+            }
+
+            _context.Races.Remove(race);
+            await _context.SaveChangesAsync();
+
+            return race;
+        }
+
+        private bool RaceExists(int id)
+        {
+            return _context.Races.Any(e => e.Id == id);
+        }
+    }
+}
