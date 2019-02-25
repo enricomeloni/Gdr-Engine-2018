397fb0879678d98ef763f71dd04277b862ad8d84
rzippo <raffaele.zippo@live.it>
2019-02-25 13:58:01 +0100

    Configure database project for migrations via design time tools

---
+++ GdrEngineNet/GdrEngineNet.Database/GdrDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GdrEngineNet.Database
+{
+    class GdrDbContextFactory : IDesignTimeDbContextFactory<GdrDbContext>
+    {
+        public GdrDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<GdrDbContext>();
+            optionsBuilder.UseMySQL(Environment.GetEnvironmentVariable("GdrEngineDb"));
+
+            return new GdrDbContext(optionsBuilder.Options);
+        }
+    }
+}
